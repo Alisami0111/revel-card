cfaa991b90de7bda036f9026b1cf5c030a926189
alisami0111 <saminagri0111@gmail.com>
2024-04-19 17:40:24 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+
+  return (
+    <div className="book">
+      <p>My name is Ali Sami I am a student of cyber security and a Web developer</p>
+      <div className="cover">
+        <p>Hello</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
